f77fd2f9554479498a574da2b8b69ee04cd92834
NOOMA-42 <suncloud2203357667@gmail.com>
2021-10-29 22:50:20 +0800

    update ble py

--- ble_scan_connect.py
+++ ble_scan_connect.py
@@ -1,13 +1,20 @@
 from bluepy.btle import Peripheral, UUID
 from bluepy.btle import Scanner, DefaultDelegate
 import struct
-"""
-DefaultDelegate receive Bluetooth message asynchronously, 
+NOTIF_ON = struct.pack('<bb', 0x01, 0x00)
+NOTIF_OFF = struct.pack('<bb', 0x00, 0x00)
 
-Scanner is to scan for LE devices which are broadcasting data
-withDelegate stores a reference to a delegate object, which receives callbacks when broadcasts from devices are received
-"""
+# user config
+DEVICE_NAME = "Heart Rate"
+SERVICE_UUID = 0xfff0
+CHAR_UUID = 0xfff1
+
 class ScanDelegate(DefaultDelegate):
+	"""
+	DefaultDelegate receive Bluetooth message asynchronously, 
+	Scanner is to scan for LE devices which are broadcasting data
+	withDelegate stores a reference to a delegate object, which receives callbacks when broadcasts from devices are received
+	"""
 	def __init__(self):
 		DefaultDelegate.__init__(self)
 	def handleDiscovery(self, dev, isNewDev, isNewData):
@@ -31,15 +38,20 @@ class PeripheralDelegate(DefaultDelegate):
 
 scanner = Scanner().withDelegate(ScanDelegate()) 
 devices = scanner.scan(3.0)
-n=0
+n = 0
 for dev in devices:
 	print ("%d: Device %s (%s), RSSI=%d dB" % (n, dev.addr, dev.addrType, dev.rssi))
 	n += 1
-	for (adtype , desc , value) in dev.getScanData():
-		print (" %s = %s" % (desc, value))
+	for (adtype, desc, value) in dev.getScanData():
+		if (value == DEVICE_NAME):
+			print('===============================')
+			print ("%s, %s" % (desc, value))
+			print('===============================')
+		else:
+			print ("%s, %s" % (desc, value))
 number = input('Enter your device number: ')
 print('Device', number)
-print(devices[number]. addr)
+print(devices[number].addr)
 
 print ("Connecting...")
 dev = Peripheral(devices[number].addr, 'random')
@@ -48,31 +60,16 @@ print ("Services...")
 for svc in dev.services:
 	print (str(svc))
 try:
-	print (str(dev.getServiceByUUID(UUID(0xfff0))))
-	testService = dev.getServiceByUUID(UUID(0xfff0))
-	for ch in testService.getCharacteristics():
-		print (str(ch))
-			
-	ch = dev.getCharacteristics(uuid=UUID(0xfff1))[0] # = ch.valHandle
-	#if (ch.supportsRead()):
-	#	print (ch.read())
-	print(ch.valHandle+1)
-	notifOn = b"\x01\x00"
-	notifOff = b"\x01\x00"
-	print(dev.readCharacteristic(0x00b7)) #
-	dev.writeCharacteristic(ch.valHandle, b"\x01\x00")
-	#print(dev.readCharacteristic(0x00b8)) #
-	#dev.writeCharacteristic(0x00b8, struct.pack('<bb', 0x01, 0x00))
-	#dev.writeCharacteristic(0x00b8, notifOn)
-	custom_service_handle_cccd = ch.valHandle + 1 #??? why 1 not 2
-	print("len: ", len(dev.getCharacteristics(uuid=UUID(0xfff1))))
+	print (str(dev.getServiceByUUID(UUID(SERVICE_UUID))))
+	ch = dev.getCharacteristics(uuid=UUID(CHAR_UUID))[0] # = ch.valHandle
+	dev.writeCharacteristic(ch.valHandle, b"\x65\x66") # test writeCharacteristic
+	custom_service_handle_cccd = ch.valHandle + 1
+	dev.writeCharacteristic(custom_service_handle_cccd, NOTIF_ON)
 	dev = dev.withDelegate(PeripheralDelegate(custom_service_handle_cccd))
-	dev.writeCharacteristic(custom_service_handle_cccd, notifOn)
-	#ch.write(bytes("Hello world\n"))
 
 	while True:
 		if dev.waitForNotifications(3.0):
-			print("test notification")
+			print("Test Notification")
 			continue
 		print ("Waiting...")
 finally:
